ecf5f62bf9c1e3515043991107b27de613b7f217
mf0x <mikefox9000@gmail.com>
2023-11-26 15:47:55 -0500

    clean up Grid.jsx

--- src/screens/Grid.jsx
+++ src/screens/Grid.jsx
@@ -1,22 +1,9 @@
-import { useState, useEffect } from "react";
-import { getAll } from "../services/actions";
 import Thumbnail from "../components/Thumbnail";
 
 function Grid({ filteredThumbnails }) {
-  // const [thumbnails, setThumbnails] = useState([]);
-
-  // useEffect(() => {
-  //   fetchThumbnails();
-  // }, []);
-
-  // async function fetchThumbnails() {
-  //   const allThumbnails = await getAll();
-  //   setThumbnails(allThumbnails);
-  // }
-
   return (
     <div>
-      <h1>All 100 images</h1>
+      <h1>All Images</h1>
       <div className="grid">
         {filteredThumbnails.map((thumbnail) => (
           <Thumbnail thumbnail={thumbnail} />
